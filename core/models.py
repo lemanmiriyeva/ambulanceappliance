b5b0b13952e05e4f76357f86d97187903c05a560
unknown <leman.miriyeva02@mail.ru>
2023-01-20 23:49:34 +0400

    initial commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Quotes(models.Model):
+    name=models.CharField(max_length=255)
+    phone=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    address=models.CharField(max_length=255)
+    zip_code=models.CharField(max_length=255)
