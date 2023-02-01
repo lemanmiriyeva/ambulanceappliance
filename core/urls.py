be48388676e2b09f37d6e78fdd4ecfa90d66922f
unknown <leman.miriyeva02@mail.ru>
2023-02-01 12:01:21 +0400

    initial commit

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,9 @@ from .models import *
 from .views import *
 
 urlpatterns=[
-    path('home/',home,name='home'),
-    path('about/',about,name='about'),
+    # path('home/',home,name='home'),
+    path('home/',HomeView.as_view(),name='home'),
+    # path('about/',about,name='about'),
+    path('about/',AboutView.as_view(),name='about'),
     path('contact/',contact,name='contact')
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import *
  
-def home(request):
-    context ={}
- 
-    context["services"] = Service.objects.all()
-    context["works"]=Works.objects.all()
-    print(context["services"],context["works"])
+#Functional view
+# def home(request):
+#     context ={}
+#     context["services"] = Service.objects.all()[:4:3]
+#     context["works"]=Works.objects.filter(title="Work1")
+#     print(context["works"])
+#     context["abouts"]=About.objects.all()
+#     print(context["services"],context["works"])
          
-    return render(request, "index.html", context)
+#     return render(request, "index.html", context)
 
+class HomeView(ListView):
+    model=Service
+    template_name="index.html"
 
-def about(request):
-    return render(request,'about.html')
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['services']=Service.objects.all()
+        context['works']=Works.objects.all()
+        context['abouts']=About.objects.all()
+        return context
+
+#Functional view
+# def about(request):
+#     context={}
+#     context["abouts"]=About.objects.all()
+#     return render(request,'about.html',context)
+
+#Generic view
+class AboutView(ListView):
+    model=About
+    template_name="about.html"
+    context_object_name="abouts"
 
 
 def contact(request):
